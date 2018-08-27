e29879e1b8cac013a5c0d8dcc997e3f7c659bd19
Lichangcong <lichangcong@baicizhan.com>
2018-08-27 18:00:19 +0800

    Init

---
+++ testProject/class/BCZPlusObject.h
@@ -0,0 +1,15 @@
+//
+//  BCZPlusObject.h
+//  testProject
+//
+//  Created by Live on 2018/8/27.
+//  Copyright © 2018年 Live. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCZPlusObject : NSObject
+
+- (NSInteger)plusA:(NSInteger)a b:(NSInteger)b;
+
+@end
---
+++ testProject/class/BCZPlusObject.m
@@ -0,0 +1,15 @@
+//
+//  BCZPlusObject.m
+//  testProject
+//
+//  Created by Live on 2018/8/27.
+//  Copyright © 2018年 Live. All rights reserved.
+//
+
+#import "BCZPlusObject.h"
+
+@implementation BCZPlusObject
+- (NSInteger)plusA:(NSInteger)a b:(NSInteger)b {
+    return a+b;
+}
+@end
