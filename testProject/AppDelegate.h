756359a511c4dc605170156372f604b7138b0c7e
Lichangcong <lichangcong@baicizhan.com>
2018-08-27 17:35:42 +0800

    Initial Commit

---
+++ testProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by Live on 2018/8/27.
+//  Copyright © 2018年 Live. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testProject
+//
+//  Created by Live on 2018/8/27.
+//  Copyright © 2018年 Live. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
